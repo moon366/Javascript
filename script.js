30eea718488f9c849200ecf2e1f036fa8b7a140d
moon366 <aayusaneupane49@gmail.com>
2026-02-10 05:59:31 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// var, let and const in js
+var a=10;
+var a=20;
+console.log(a)
+// Var is global scoped that can be re-declared
+// Let is block scoped variable that cannot be re-declared
+let b=20;
+let b=30;
